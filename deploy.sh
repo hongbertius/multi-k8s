bab86f6d9c3b00f3410be7ad6c648a208124c78c
hongbertius <hongbert@gmail.com>
2020-02-20 04:13:32 +0000

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
-docker build -t nottidock/multi-client:latest -t nottidock/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t nottidock/multi-server:latest -t nottidock/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t nottidock/multi-worker:latest -t nottidock/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t nottidock/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nottidock/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nottidock/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push nottidock/multi-client:latest
 docker push nottidock/multi-client:$SHA
-docker push nottidock/multi-server:latest
 docker push nottidock/multi-server:$SHA
-docker push nottidock/multi-worker:latest
 docker push nottidock/multi-worker:$SHA
 
 kubectl apply -f k8s
