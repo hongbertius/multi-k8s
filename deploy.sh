190c1f2e783c8ae7da443846441a597dd764a716
hongbertius <hongbert@gmail.com>
2020-02-20 04:22:27 +0000

    added language to travis yaml

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
-docker build -t nottidock/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t nottidock/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t nottidock/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t nottidock/multi-client:latest -t nottidock/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nottidock/multi-server:latest -t nottidock/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nottidock/multi-worker:latest -t nottidock/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push nottidock/multi-client:$SHA
 docker push nottidock/multi-server:$SHA
 docker push nottidock/multi-worker:$SHA
+docker push nottidock/multi-client:latest
+docker push nottidock/multi-server:latest
+docker push nottidock/multi-worker:latest
 
 kubectl apply -f k8s
 
